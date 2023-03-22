118a8746b831cbe98e2b7b5d86224a584a294b97
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-03-22 17:30:24 +0200

    Add authentification

---
+++ URLShortener/URLShortener/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using URLShortener.Domain.Models;
+
+namespace URLShortener.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (!ModelState.IsValid) return View(loginModel);
+
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginModel.Password, true, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    TempData["Error"] = "Wrong credentials. Please try again";
+                    return View(loginModel);
+                }
+            }
+            TempData["Error"] = "Wrong credentials. Please try again";
+            return View(loginModel);
+
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
